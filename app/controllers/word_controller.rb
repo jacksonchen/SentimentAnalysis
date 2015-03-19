277ddc20ad4d011849a5ab7cffe57f2a1e72d474
Jackson Chen <tiger2chen@gmail.com>
2015-03-18 23:00:24 -0600

    Remote csv grab

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -1,64 +1,67 @@
+require 'csv'
+require 'open-uri'
+
 class WordController < ApplicationController
   def index
 
   end
 
   def processing
-    if params[:formtype] == "upload"
-      file_data = params[:uploaded_file]
+    if Value.count.zero?
+      file_data = open("http://www.chenjackson.com/word_values.csv")
       if file_data.respond_to?(:read)
         database_setup(file_data)
-        flash[:success] = "Upload successful"
-        render :index
+        #flash[:success] = "Upload successful"
+        #render :index
       else
         flash[:error] = "File error"
-        redirect_to root_path
+        #redirect_to root_path
+      end
+    end
+
+    words = params[:word_string].split(/[\s,.;]/)
+    words.each do |word|
+      word = word.gsub(/[\.,;]/,'')
+      if word.empty?
+        words.delete(word)
+      end
+    end
+    value = 0
+    words.each do |theword|
+      thevalue = Value.where(word: theword)[0]
+      if !thevalue.nil?
+        value = value + thevalue.value
+      end
+    end
+    flash[:alert] = "Your string: " + params[:word_string]
+    if value < 0
+      if value < 1
+        flash[:success] = "Predicted mood: Happy"
+      else
+        flash[:success] = "Predicted mood: Very joyous"
+      end
+    elsif value > 0
+      if value > -1
+        flash[:success] = "Predicted mood: Grumpy"
+      else
+        flash[:success] = "Predicted mood: Mad"
       end
     else
-      words = params[:word_string].split(/[\s,.;]/)
-      words.each do |word|
-        word = word.gsub(/[\.,;]/,'')
-        if word.empty?
-          words.delete(word)
-        end
-      end
-      value = 0
-      words.each do |theword|
-        thevalue = Value.where(word: theword)[0]
-        if !thevalue.nil?
-          value = value + thevalue.value
-        end
-      end
-      flash[:alert] = "Your string: " + params[:word_string]
-      if value < 0
-        if value < 1
-          flash[:success] = "Predicted mood: Happy"
-        else
-          flash[:success] = "Predicted mood: Very joyous"
-        end
-      elsif value > 0
-        if value > -1
-          flash[:success] = "Predicted mood: Grumpy"
-        else
-          flash[:success] = "Predicted mood: Mad"
-        end
-      else
-        flash[:success] = "Predicted mood: Neutral"
-      end
-      render :index
+      flash[:success] = "Predicted mood: Neutral"
     end
+    render :index
 
   end
 
   private
 
   def database_setup(file_data)
-    file_contents = file_data.read
+    file_contents = CSV.parse(file_data)
     Value.delete_all
-    file_contents.each_line do |line|
+    file_contents.each do |line|
       word_value = Value.new()
-      word_value.word = /^[^,]+/.match(line)
-      word_value.value = /,(\S+)/.match(line).captures[0]
+      word_value.word = line[0]
+      word_value.value = line[1]
       word_value.save
     end
   end
